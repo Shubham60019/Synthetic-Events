6ee81a4ae8037314a63a78948004c7c7468756c4
Shubham Sharma <Shubham60019@gmail.com>
2023-09-05 23:55:56 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <button onClick={(e)=>{
+        console.log(e);
+    }}>Login</button>
+    </>
+  )
+}
+
+export default App
